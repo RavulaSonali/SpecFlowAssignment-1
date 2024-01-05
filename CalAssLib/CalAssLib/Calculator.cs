94a734c22897ce0adb127a69a598ae95b5bf5e9d
Ravula Sonali <ravula.sonali@gmail.com>
2024-01-05 15:56:04 +0530

    SpecFlowAssignment-1

---
+++ CalAssLib/CalAssLib/Calculator.cs
@@ -0,0 +1,30 @@
+﻿namespace CalAssLib
+{
+    public class Calculator
+    {
+
+        public int Fnum { get; set; }
+
+        public int Snum { get; set; }
+
+        public int Sub()
+        {
+            int res;
+            res = Fnum - Snum;
+            return res;
+        }
+        public int Mul()
+        {
+            int res;
+            res = Fnum * Snum;
+            return res;
+        }
+        public int Div()
+        {
+            int res;
+            res = Fnum / Snum;
+            return res;
+        }
+
+    }
+}
